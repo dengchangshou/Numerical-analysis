2c8fdd61a1be428179748e2b09eb6bc6fb5e2c64
Rock <2412445816@qq.com>
2017-06-01 10:52:33 +0800

    二分法

---
+++ bisection.m
@@ -0,0 +1,45 @@
+% 二分法
+function [r] = bisection(f,a,b,N,eps_step,eps_abs)
+    % Check that neither end-point is a root
+    % and if f(a) and f(b) have the same sign, throw an exception
+
+    if(f(a) == 0)
+        r = a;
+        return;
+    elseif (f(a) * f(b) > 0)
+        error('f(a) and f(b) do not have oppsite signs');
+    end
+
+    % We will iterate N times and if a root was not
+    % found after N iterations, an exception will be thrown.
+
+    for k = 1:N
+        % Find the mid-point
+        c = (a+b)/2;
+        % Check if we found a root or whether or not
+        % we should continue with:
+        % [a,c] if f(a) and f(b) have opposite signs, or
+        % [c,b] if f(a) and f(b) have opposite signs.
+        if (f(c) == 0)
+            r = c;
+        return;
+        elseif (f(c)*f(a) < 0)
+            b = c;
+        else
+            a = c;
+        end
+        % if |b-a|<eps_step, check whether or not
+        % |f(a)|<|f(b)| and |f(a)|<eps_abs and return 'a', or
+        % |f(b)|<eps_abs and return 'b'.
+        if (abs(b-a) < eps_step)
+            if(abs(f(a)) < abs(f(b)) && abs(f(a)) < eps_abs)
+                r = a;
+                return;
+            elseif (abs(f(b)) < eps_abs)
+                r = b;
+                return;
+            end
+        end
+    end
+    error('the method did not converge');
+end
